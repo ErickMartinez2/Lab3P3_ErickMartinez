b2294b9956fe16ec322bfe7ae2c5548fa926c058
ErickMartinez2 <erickmartinez2203@gmail.com>
2017-08-04 13:46:42 -0600

    Creacion de Matrices

--- Lab3P3_ErickMartinez.cpp
+++ Lab3P3_ErickMartinez.cpp
@@ -1,16 +1,117 @@
 #include <iostream>
 using namespace std;
 
-int** CrearMatriz1_j1();
-int** CrearMatriz2_j1();
-int** CrearMatriz1_j2();
-int** CrearMatriz2_j2();
+char** CrearMatriz1_j1();
+char** CrearMatriz2_j1();
+char** CrearMatriz1_j2();
+char** CrearMatriz2_j2();
 
 int main() {
 	cout << "-> BATTLESHOTS" << endl;
-	int** matriz1_j1 = CrearMatriz1_j1();
-	int** matriz2_j1 = CrearMatriz2_j1();
-	int** matriz1_j2 = CrearMatriz1_j2();
-	int** matriz2_j2 = CrearMatriz2_j2();
+	char** matriz1_j1 = CrearMatriz1_j1();
+	char** matriz2_j1 = CrearMatriz2_j1();
+	char** matriz1_j2 = CrearMatriz1_j2();
+	char** matriz2_j2 = CrearMatriz2_j2();
 	return 0;
+}
+
+char** CrearMatriz1_j1() {
+	char** matriz = new char*[8];
+	for (int i = 0; i < 8; i++) {
+		matriz[i] = new char[8];
+	}
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			if ((i == 1) && (j > 0 && j < 4)) {
+				matriz[i][j] = '*';
+			} else {
+				if ((i == 2) && (j == 7)) {
+					matriz[i][j] = '*';
+				} else {
+					if ((i == 3) && (j == 4 || j == 7)) {
+						matriz[i][j] = '*';
+					} else {
+						if ((i == 4) && (j == 4 || j == 7)) {
+							matriz[i][j] = '*';
+						} else {
+							if ((i == 5) && (j == 4)) {
+								matriz[i][j] = '*';
+							} else {
+								if ((i == 7) && (j > 0 && j < 4)) {
+									matriz[i][j] = '*';
+								}else{
+									matriz[i][j] = ' ';
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	return matriz;
+}
+
+char** CrearMatriz2_j1() {
+	char** matriz = new char*[8];
+	for (int i = 0; i < 8; i++) {
+		matriz[i] = new char[8];
+	}
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			matriz[i][j] = ' ';
+		}
+	}
+	return matriz;
+}
+
+char** CrearMatriz1_j2() {
+	char** matriz = new char*[8];
+	for (int i = 0; i < 8; i++) {
+		matriz[i] = new char[8];
+	}
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			if ((i == 0) && (j > 0 && j < 4)) {
+				matriz[i][j] = '*';
+			} else {
+				if ((i == 3) && (j > 4 && j <= 7)) {
+					matriz[i][j] = '*';
+				} else {
+					if ((i == 4) && (j == 4)) {
+						matriz[i][j] = '*';
+					} else {
+						if ((i == 5) && (j == 1 || j == 4)) {
+							matriz[i][j] = '*';
+						} else {
+							if ((i == 6) && (j == 1 || j == 4)) {
+								matriz[i][j] = '*';
+							} else {
+								if ((i == 7) && (j == 1)) {
+									matriz[i][j] = '*';
+								}else{
+									matriz[i][j] = ' ';
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	return matriz;
+
+}
+
+char** CrearMatriz2_j2() {
+	char** matriz = new char*[8];
+	for (int i = 0; i < 8; i++) {
+		matriz[i] = new char[8];
+	}
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			matriz[i][j] = ' ';
+		}
+	}
+	return matriz;
 }
